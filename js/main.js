0c807042ad1404da1a439d10e9c9094d51794a43
ShubhamKumar37 <sk9818281820@gmail.com>
2025-01-26 23:51:45 +0530

    add contact form and footer section with responsive styling

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 import { navLinks } from "./data.js";
 
 const navLinksContainer = document.querySelector(".nav-links");
+const footerNavLinksContainer = document.querySelector(".footer-section .nav-links");
 
 (function navLinkRender() {
     const allLinks = navLinks.map((link) => {
         return `<li><a href="#">${link}</a></li>`;
     }).join("");
 
+    // Render links in the header's nav
     navLinksContainer.innerHTML = allLinks;
+
+    // Render links in the footer's nav
+    if (footerNavLinksContainer) {
+        footerNavLinksContainer.innerHTML = allLinks;
+    }
 })();
